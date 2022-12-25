600591db590f381caf41d2229fb7732ce0291504
qqKostya <leap74rus@gmail.com>
2022-12-25 10:12:37 +0300

    lesson 59 homework

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -13,8 +13,44 @@ const ProfileInfo = (props) => {
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4YWwJaZ79oIELX6C7q_QtBT3Doab4yGxcvQ&usqp=CAU" />
       </div>
       <div className={classes.description_block}>
-        <img src={props.profile.photos.small} />
-        ava + descr
+        <div>
+          <h2>{props.profile.fullName}</h2>
+          <img src={props.profile.photos.small} />
+          <p>{props.profile.aboutMe}</p>
+        </div>
+        <div>
+          <span>{props.profile.lookingForAJob ? "=)" : "=("}</span>
+          <span>{props.profile.lookingForAJobDescription}</span>
+        </div>
+        <div>
+          <h3>My contacts:</h3>
+          <ul>
+            <li>
+              <a href={props.profile.facebook}>facebook</a>
+            </li>
+            <li>
+              <a href={props.profile.website}>website</a>
+            </li>
+            <li>
+              <a href={props.profile.vk}>vk</a>
+            </li>
+            <li>
+              <a href={props.profile.twitter}>twitter</a>
+            </li>
+            <li>
+              <a href={props.profile.instagram}>instagram</a>
+            </li>
+            <li>
+              <a href={props.profile.youtube}>youtube</a>
+            </li>
+            <li>
+              <a href={props.profile.github}>github</a>
+            </li>
+            <li>
+              <a href={props.profile.mainLink}>mainLink</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
