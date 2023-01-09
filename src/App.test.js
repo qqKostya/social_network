6e781ee1882fbdc5e8ce049ba6b2ee0332c98577
qqKostya <leap74rus@gmail.com>
2023-01-09 07:37:13 +0300

    lesson 92

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders without crashing", () => {
+  const container = document.createElement("div");
+  const root = createRoot(container);
+  root.render(<App tab="home" />);
+  root.unmount();
 });
