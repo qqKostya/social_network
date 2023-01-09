d689f8aedb1d1f1e3d5594f1b747301369dd46d0
qqKostya <leap74rus@gmail.com>
2023-01-09 08:10:21 +0300

    lesson 93-1

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,26 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Paginator.module.css";
 
-const Paginator = (props) => {
-  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+const Paginator = ({
+  totalItemsCount,
+  pageSize,
+  currentPage,
+  onPageChanged,
+  portionSize = 10,
+}) => {
+  let pagesCount = Math.ceil(totalItemsCount / pageSize);
 
   let pages = [];
 
   for (let i = 1; i <= pagesCount; i++) {
     pages.push(i);
   }
+
+  let portionCount = Math.ceil(pagesCount / portionSize);
+  let [portionNumber, setPortionNumber] = useState(1);
+  let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+  let rightPortionPageNumber = portionNumber * portionSize;
+
   return (
-    <div>
-      {pages.map((page) => (
-        <span
-          className={props.currentPage === page && styles.selectedPage}
+    <div className={styles.paginator}>
+      {portionNumber > 1 && (
+        <button
           onClick={() => {
-            props.onPageChanged(page);
+            setPortionNumber(portionNumber - 1);
           }}
         >
-          {page}
-        </span>
-      ))}
+          PREV
+        </button>
+      )}
+
+      {pages
+        .filter(
+          (p) => p >= leftPortionPageNumber && p <= rightPortionPageNumber
+        )
+        .map((p) => {
+          return (
+            <span
+              className={
+                currentPage === p ? styles.selectedPage : styles.pageNumber
+              }
+              key={p}
+              onClick={(e) => {
+                onPageChanged(p);
+              }}
+            >
+              {p}
+            </span>
+          );
+        })}
+      {portionCount > portionNumber && (
+        <button
+          onClick={() => {
+            setPortionNumber(portionNumber + 1);
+          }}
+        >
+          NEXT
+        </button>
+      )}
     </div>
   );
 };
