b4f81d92a9825eb872cce56387619fcf87cf809c
qqKostya <leap74rus@gmail.com>
2023-01-16 06:34:06 +0300

    2.0 lesson 5-2

---
+++ src/components/Users/Users.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Paginator from "../common/Paginator/Paginator";
+import User from "./User";
+import { UsersType } from "../../types/types"
+
+type PropsType = {
+  currentPage: number
+  onPageChanged: () => void
+  totalUsersCount: number
+  pageSize: number
+  folowingInProgress: Array<number>
+  unfollow: () => void
+  follow: () => void
+  users: Array<UsersType>
+}
+
+const Users: React.FC<PropsType> = ({ currentPage, onPageChanged, totalUsersCount, pageSize, folowingInProgress, unfollow, follow, users }) => {
+  return (
+    <div>
+      <Paginator
+        currentPage={currentPage}
+        onPageChanged={onPageChanged}
+        totalItemsCount={totalUsersCount}
+        pageSize={pageSize}
+      />
+      <div>
+        {users.map((u) => (
+          <User
+            user={u}
+            folowingInProgress={folowingInProgress}
+            unfollow={unfollow}
+            follow={follow}
+            key={u.id}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Users;
