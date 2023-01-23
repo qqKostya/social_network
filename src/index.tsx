76061456d7c18f05cfcb8a6c6ae192b751725b18
qqKostya <leap74rus@gmail.com>
2023-01-23 07:27:16 +0300

    2.0 lesson 11-15 index migration tsx

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <HashRouter>
