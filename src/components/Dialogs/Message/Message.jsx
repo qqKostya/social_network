4edbcff5bc37cdcb512d4add2d72c3feaabb098b
qqKostya <leap74rus@gmail.com>
2023-01-23 07:10:07 +0300

    2.0 lesson 11-13 Message and Dialogitem migration tsx

--- src/components/Dialogs/Message/Message.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-import classes from "./Message.module.css";
-const Message = (props) => {
-  return <div className={classes.message}>{props.message}</div>;
-};
-
-export default Message;
