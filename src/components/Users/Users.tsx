508f025f3f390c6c1240aadaef6578f1a149bf82
qqKostya <leap74rus@gmail.com>
2023-01-28 12:12:07 +0300

    2.0 lesson 16 add query string to Users

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -6,6 +6,7 @@ import { FilterType, getUsers, follow as followReducer, unfollow as unfollowRedu
 import { useDispatch, useSelector } from "react-redux";
 import { getCurrentPage, getFolowingInProgress, getPageSize, getTotalUsersCount, getUsersFilter, getUsersSuperSelector } from "../../redux/users-selectors";
 import { AppDispatch } from "../../redux/redux-store";
+import { useSearchParams } from "react-router-dom";
 
 type PropsType = {}
 
@@ -20,9 +21,56 @@ const Users: React.FC<PropsType> = () => {
 
   const dispatch: AppDispatch = useDispatch()
 
+  const [searchParams, setSearchParams] = useSearchParams()
+
+
   useEffect(() => {
-    dispatch(getUsers(currentPage, pageSize, filter))
+
+    const result: any = {}
+    // @ts-ignore
+    for (const [key, value] of searchParams.entries()) {
+      let value2: any = +value
+      if (isNaN(value2)) {
+        value2 = value
+      }
+      if (value === 'true') {
+        value2 = true
+      } else if (value === 'false') {
+        value2 = false
+      }
+      result[key] = value2
+    }
+
+    let actualPage = result.page || currentPage
+    let term = result.term || filter.term
+
+    let friend = result.friend || filter.friend
+    if (result.friend === false) {
+      friend = result.friend
+    }
+
+    const actualFilter = { friend, term }
+
+    dispatch(getUsers(actualPage, pageSize, actualFilter))
+
+    // eslint-disable-next-line
   }, [])
+
+
+  useEffect(() => {
+
+    const term = filter.term
+    const friend = filter.friend
+
+    let urlQuery =
+      (term === '' ? '' : `&term=${term}`)
+      + (friend === null ? '' : `&friend=${friend}`)
+      + (currentPage === 1 ? '' : `&page=${currentPage}`)
+
+    setSearchParams(urlQuery)
+
+    // eslint-disable-next-line
+  }, [filter, currentPage])
 
   const onPageChanged = (pageNumber: number) => {
     dispatch(getUsers(pageNumber, pageSize, filter))
