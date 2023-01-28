e1d9c06dd97a827eb390ffb660f260be42f20bd5
qqKostya <leap74rus@gmail.com>
2023-01-28 14:24:17 +0300

    2.0 lesson 17 add antd and components header and notfound

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, {Suspense} from "react";
+import { Routes, Route, Link, Navigate } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
 import News from "./components/News/News";
-import Music from "./components/Music/Music";
-import Settings from "./components/Settings/Settings";
+
 import UsersPage from "./components/Users/UsersContainer";
-import HeaderContainer from "./components/Header/HeaderContainer";
+import {HeaderComponent} from "./components/Header/HeaderComponent";
 import Login from "./components/Login/Login";
 import { initializeApp } from "./redux/app-reducer";
 import { connect } from "react-redux";
 import Preloader from "./components/common/Preloader/Preloader";
 import withSuspense from "./hoc/withSuspens";
 import { AppStateType } from "./redux/redux-store";
+
+
+import { Breadcrumb, Layout, Menu } from 'antd'
+import type { MenuProps } from 'antd/es/menu'
+import { LaptopOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons'
+import { NotFound } from "./components/common/NotFound/NotFound";
 
 const ProfileContainer = withSuspense(
   React.lazy(() => import("./components/Profile/ProfileContainer"))
@@ -26,6 +30,64 @@ type MapPropsType = ReturnType<typeof mapStateToProps>
 type DispatchPropsType = {
   initializeApp: () => void
 }
+
+
+//region antd
+const { Content, Footer, Sider } = Layout
+
+export type MenuItem = Required<MenuProps>['items'][number];
+
+export function getItem(label: React.ReactNode,
+  key?: React.Key | null,
+  icon?: React.ReactNode,
+  children?: MenuItem[]): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label
+  } as MenuItem
+}
+
+
+const itemsSideMenu: MenuItem[] = [
+
+  getItem('My Profile', 'MyProfile', <UserOutlined />, [
+    getItem(
+      <Link to='/profile'>
+        Profile
+      </Link>,
+      'Profile'),
+    getItem(
+      <Link to='/dialogs'>
+        Messages
+      </Link>,
+      'Messages')
+  ]),
+
+  getItem('Developers', 'Developers', <LaptopOutlined />, [
+    getItem(
+      <Link to='/developers'>
+        Developers list
+      </Link>,
+      'DevelopersList'),
+    getItem('Developers chat', 'DevelopersChat')
+  ]),
+
+  getItem('Settings', 'Settings', <SettingOutlined />, [
+    getItem(
+      <Link to="/news">
+        News
+      </Link>,
+      'News'),
+    getItem(
+      <Link to="/22404">
+        Music
+      </Link>,
+      'Music')
+  ])
+]
+//endregion antd
 
 class App extends React.Component<MapPropsType & DispatchPropsType> {
   // cathcAllUnhandleErrors = (promiseRejectionEvent) => {
@@ -55,27 +117,85 @@ class App extends React.Component<MapPropsType & DispatchPropsType> {
     }
 
     return (
-      <div className="app-wrapper">
-        <HeaderContainer />
-        <Navbar />
-        <div className="app-wrapper-content">
-          <Routes>
-            <Route path="/" element={<ProfileContainer />} />
-            <Route path="/profile/:userId" element={<ProfileContainer />} />
-            <Route path="/profile" element={<ProfileContainer />} />
-            <Route path="/dialogs/*" element={<DialogsContainer />} />
-            <Route path="/news" element={<News />} />
-            <Route path="/music" element={<Music />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route
-              path="/users"
-              element={<UsersPage pageTitle={"Samurais"} />}
-            />
-            <Route path="/login" element={<Login />} />
-            <Route path="*" element={<div>404 NOT FOUND</div>} />
-          </Routes>
-        </div>
-      </div>
+      <Layout>
+        <HeaderComponent />
+        <Content style={{ padding: '0 50px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>
+              <Link to='/'>
+                Home
+              </Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>
+              <Link to='/developers'>
+                List
+              </Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
+            <Sider className="site-layout-background" width={200}>
+              <Menu mode="inline" style={{ height: '100%' }} items={itemsSideMenu} />
+            </Sider>
+            <Content style={{ padding: '0 24px', minHeight: 280 }}>
+              <Suspense fallback={<Preloader />}>
+                <Routes>
+                  <Route
+                    path="/"
+                    element={<Navigate to="/profile" />} />
+                  <Route
+                    path='/profile/:userId'
+                    element={<ProfileContainer />} />
+                  <Route
+                    path='/profile'
+                    element={<ProfileContainer />} />
+                  <Route />
+                  <Route
+                    path='/dialogs/*'
+                    element={<DialogsContainer />} />
+                  <Route
+                    path='/developers'
+                    element={<UsersPage
+                      pageTitle={'Самураи'} />} />
+                  <Route
+                    path='/login'
+                    element={<Login />} />
+                  <Route
+                    path='/news'
+                    element={<News />} />
+                  <Route
+                    path='*'
+                    element={<NotFound />} />
+                </Routes>
+              </Suspense>
+            </Content>
+          </Layout>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Samurai Social Network ©2022 Created by IT-KAMASUTRA/Maxim Saharov
+        </Footer>
+      </Layout>
+      // <div className="app-wrapper">
+      //   <HeaderContainer />
+      //   <Navbar />
+      //   <div className="app-wrapper-content">
+      //     <Routes>
+      //       <Route path="/" element={<ProfileContainer />} />
+      //       <Route path="/profile/:userId" element={<ProfileContainer />} />
+      //       <Route path="/profile" element={<ProfileContainer />} />
+      //       <Route path="/dialogs/*" element={<DialogsContainer />} />
+      //       <Route path="/news" element={<News />} />
+      //       <Route path="/music" element={<Music />} />
+      //       <Route path="/settings" element={<Settings />} />
+      //       <Route
+      //         path="/users"
+      //         element={<UsersPage pageTitle={"Samurais"} />}
+      //       />
+      //       <Route path="/login" element={<Login />} />
+      //       <Route path="*" element={<div>404 NOT FOUND</div>} />
+      //     </Routes>
+      //   </div>
+      // </div>
     );
   }
 }
